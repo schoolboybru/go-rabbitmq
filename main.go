eb5e9eec906adb864596301f7f1a4aabb25206a3
Brandon Bachynski <brandon.bachynski@gmail.com>
2021-07-19 23:50:30 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Successfully Connected to our rabbitmq instance")
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(q)
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println("Successfully published message to queue")
+
+}
